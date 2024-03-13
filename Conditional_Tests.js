cd6df11625238c17571dea5f22c134c7dc8aecda
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:35:30 +0500

    first commit

---
+++ Conditional_Tests.js
@@ -0,0 +1,34 @@
+"use strict";
+//Write a series of conditional tests.
+//Creating car veriable.
+let car = 'civic';
+// Test no 1.
+console.log("Is car === 'civic'? I piredict True.");
+console.log(car === 'civic');
+// Test no 2.
+console.log("Is car == 'civic'? I piredict True.");
+console.log(car == 'civic');
+// Test no 3.
+console.log("Is car != 'Prado'? I piredict True.");
+console.log(car != 'Prado');
+// Test no 4.
+console.log("Is car !== 'Prado'? I piredict True.");
+console.log(car !== 'Prado');
+// Test no 5.
+console.log("Is car !== 'Alto'? I piredict True.");
+console.log(car !== 'Alto');
+// Test no 6.
+console.log("Is car = 'Alto'? I piredict True.");
+console.log(car != 'civic');
+// Test no 7.
+console.log("Is car == '804'? I piredict True.");
+console.log(car == '804');
+// Test no 8.
+console.log("Is car != 'civic'? I piredict True.");
+console.log(car != 'civic');
+// Test no 9.
+console.log("Is car === 'Prado'? I piredict True.");
+console.log(car === 'Prado');
+// Test no 10.
+console.log("Is car != 'civic'? I piredict True.");
+console.log(car != 'civic');
